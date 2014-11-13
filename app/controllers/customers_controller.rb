ebdf4d4d2bc7d702fd685e342e504641e2146f6c
Joshua Sebastian <jbs87@hotmail.com>
2014-11-13 14:02:53 -0500

    Created customer controllers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,20 @@
+class CustomersController < ApplicationController
+  def new
+  	@customer = Customer.new
+  end
+
+  def create
+  	@customer = Customer.create(user_params)
+  	if @customer.save
+  		redirect_to home_path
+  	else
+  		render :new
+  	end
+  end
+
+
+  private
+  def user_params
+  	params.require(:customer).permit(:name, :email, :password, :password_confirmation)
+  end
+end
