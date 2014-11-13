a5fc111af6c214e98433f105ee1547fe342d50e3
Joshua Sebastian <jbs87@hotmail.com>
2014-11-13 14:37:38 -0500

    Added log in link on front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root 'home#index'
   resources :customers, routes: [:new, :create]
   resources :sessions, routes: [:new, :create, :destroy]
