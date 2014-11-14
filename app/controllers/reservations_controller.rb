2ed22c43c6861f54bfb15715c105fc9c9ce66b14
Rudi Luzar <rudi@rudicode.com>
2014-11-14 07:15:14 -0500

    Refactored Reservation controller and added a Reservation#over_capacity? method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,12 +6,15 @@ class ReservationsController < ApplicationController
   def create
   	@reservation = Reservation.new(res_params)
   	@reservation.customer_id = current_customer.id
-  	@restaurant = @reservation.restaurant
-  	currcap = @restaurant.timeslots(params[:reservation][:time])
-  	binding.pry
-  	if params[:reservation][:people].to_i > (100- currcap)
-  		flash.now[:alert]= "At capacity"
-  		render 'restaurants/show'
+  	
+   #  @restaurant = @reservation.restaurant
+  	# currcap = @restaurant.timeslots(params[:reservation][:time])
+  	# binding.pry
+  	# if params[:reservation][:people].to_i > (100- currcap)
+    
+  	if @reservation.over_capacity?
+    	flash.now[:alert]= "At capacity"
+  		render 'restaurants/show' # restaurant_path(@reservation.restaurant)
   	else
   		if @reservation.save 
   			redirect_to restaurant_path(@reservation.restaurant), notice: "Made a reservation"
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,19 @@ class Reservation < ActiveRecord::Base
   belongs_to :customer
   belongs_to :restaurant
 
-   validates :people,:time, presence: true
-   validates :people, numericality: {only_integer: true}
-   validates_inclusion_of :people, :in => 2..100
+  validates :people,:time, presence: true
+  validates :people, numericality: {only_integer: true}
+  validates_inclusion_of :people, :in => 2..100
+
+   def over_capacity?
+    # should this be a validation? It seems to be validating that.
+    #the current timeslot has 100 or less people.
+    # 
+    total_people = restaurant.reservations.sum(:people) # this needs to be changed to find people only for current timeslot.
+      # the idea is to query the database for all reservations with the same time slot
+      # then use the sum method to find out how many total people.
+
+      #binding.pry
+    (total_people + people) > 100
+   end  
 end
