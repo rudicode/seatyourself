6b8bd20d586da981f2350fc809c1e02b24c2e42c
Joshua Sebastian <jbs87@hotmail.com>
2014-11-14 13:38:54 -0500

    Changed capacity check method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,15 @@
 class Reservation < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :restaurant
+	belongs_to :customer
+	belongs_to :restaurant
 
-   validates :people,:time, presence: true
-   validates :people, numericality: {only_integer: true}
-   validates_inclusion_of :people, :in => 2..100
+	validates :people,:time, presence: true
+	validates :people, numericality: {only_integer: true}
+	validates_inclusion_of :people, :in => 2..100
+
+  def available?(party_size, start_time, restaurant_id)
+    reserved = Reservation.where(:restaurant_id => restaurant_id).where(:time => start_time).sum(:people)
+    binding.pry
+    return party_size.to_i <= (100 - reserved)
+  end
+
 end
